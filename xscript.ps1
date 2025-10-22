fb8d5ff3656dc493597e1c23a3afab845a0cd79c
iamrudyard <147968558+iamrudyard@users.noreply.github.com>
2025-10-22 08:58:35 +0800

    Update uninstall script for No SaRA version

--- xscript.ps1
+++ xscript.ps1
@@ -1,16 +1,16 @@
 # ============================
-# PowerShell Office Uninstall Bootstrap (Silent)
+# PowerShell Office Uninstall Bootstrap (No SaRA)
 # ============================
 # Usage:
 # irm https://raw.githubusercontent.com/iamrudyard/office_uninstall/main/xscript.ps1 | iex
 
 try {
-    $url = 'https://raw.githubusercontent.com/iamrudyard/office_uninstall/main/uninstall_office.cmd'
+    $url = 'https://raw.githubusercontent.com/iamrudyard/office_uninstall/main/uninstall_office_nosara.cmd'
     Write-Host "[*] Downloading Office uninstaller..." -ForegroundColor Cyan
 
     $response = Invoke-RestMethod -Uri $url -UseBasicParsing
     if ($response) {
-        $tempFile = "$env:TEMP\office_uninstall.cmd"
+        $tempFile = "$env:TEMP\office_uninstall_nosara.cmd"
         Set-Content -Path $tempFile -Value $response -Encoding ASCII
         Write-Host "[*] Running Office uninstaller silently..." -ForegroundColor Yellow
         Start-Process cmd.exe -ArgumentList "/c", $tempFile -Verb RunAs -WindowStyle Hidden
